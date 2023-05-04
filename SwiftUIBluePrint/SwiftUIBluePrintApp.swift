767f114fe3c1991b3bea413ea1f3fbe4bf1f761c
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2023-05-04 10:53:37 -0300

    Initial Commit

---
+++ SwiftUIBluePrint/SwiftUIBluePrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBluePrintApp.swift
+//  SwiftUIBluePrint
+//
+//  Created by Roberto Edgar Geiss on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBluePrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
